6626aefbf2399a99bc92f35d882800e9f6b27e20
Vagusko <p.kolinko95@gmail.com>
2020-10-11 19:52:50 +0200

    change hierchie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ContactFormComponent } from './contact-form/contact-form.component';
-import { DeviceListComponent } from './device-list/device-list.component';
-import { ModuleListComponent } from './module-list/module-list.component';
-import { ModuleTypeComponent } from './module-type/module-type.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { AddUserComponent } from './add-user/add-user.component';
-import { AddDeviceComponent } from './add-device/add-device.component';
-import { AddModuleComponent } from './add-module/add-module.component';
-import { AddModuleTypeComponent } from './add-module-type/add-module-type.component';
-import { UpdateModuleTypeComponent } from './update-module-type/update-module-type.component';
-import { UpdateDeviceComponent } from './update-device/update-device.component';
-import { UpdateUserComponent } from './update-user/update-user.component';
-import { UpdateModuleComponent } from './update-module/update-module.component';
+import { DeviceListComponent } from './list/device-list/device-list.component';
+import { ModuleListComponent } from './list/module-list/module-list.component';
+import { ModuleTypeComponent } from './list/module-type-list/module-type.component';
+import { UserListComponent } from './list/user-list/user-list.component';
+import { AddUserComponent } from './add/add-user/add-user.component';
+import { AddDeviceComponent } from './add/add-device/add-device.component';
+import { AddModuleComponent } from './add/add-module/add-module.component';
+import { AddModuleTypeComponent } from './add/add-module-type/add-module-type.component';
+import { UpdateModuleTypeComponent } from './update/update-module-type/update-module-type.component';
+import { UpdateDeviceComponent } from './update/update-device/update-device.component';
+import { UpdateUserComponent } from './update/update-user/update-user.component';
+import { UpdateModuleComponent } from './update/update-module/update-module.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactFormComponent,
     DeviceListComponent,
     ModuleListComponent,
     ModuleTypeComponent,
