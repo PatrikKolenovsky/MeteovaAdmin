d9a305070ca5f55f041e0d91b1b3ed4e66ebc0ad
Vagusko <p.kolinko95@gmail.com>
2020-11-11 14:52:40 +0100

    - add module to list

--- src/app/list/module-list/module-list.component.ts
+++ src/app/list/module-list/module-list.component.ts
@@ -1,4 +1,8 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Device} from '../../model/device.model';
+import {Module} from '../../model/module';
+import {DeviceService} from '../../services/device.service';
+import {ModuleService} from '../../services/module.service';
 
 @Component({
   selector: 'app-module-list',
@@ -10,10 +14,19 @@ export class ModuleListComponent implements OnInit {
   @Output() messageEvent = new EventEmitter<string>();
 
   Content = '';
+  moduleData: Array<Module> = [];
 
-  constructor() { }
+  constructor(private moduleService: ModuleService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.moduleService.readAll()
+      .subscribe(
+        (moduleData: Array<Module>) => this.moduleData = moduleData,
+        (error) => console.log(error),
+        () => {
+        }
+      );
+  }
 
   setActiveContent(ActiveContent): void {
     this.Content = '_module' + ActiveContent;
--- src/app/services/module.service.ts
+++ src/app/services/module.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {RestApiService} from './rest-api.service';
+import {Module} from '../model/module';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModuleService {
 
-  constructor() { }
+  HTTP_API_PATH = RestApiService.HTTP_API_PATH + 'modules';
+  constructor(private http: HttpClient) { }
+
+  create(input: any): void {
+    this.http.post<any>(this.HTTP_API_PATH, {input}).subscribe();
+  }
+
+  delete(id: number): void {
+    this.http.post<any>(this.HTTP_API_PATH, {id}).subscribe();
+  }
+
+  readAll(): any {
+    return this.http.get<Array<Module>>(this.HTTP_API_PATH);
+  }
+
+  read(id: number): Observable<Module> {
+    return this.http.get<Module>(this.HTTP_API_PATH + '/' + id);
+  }
 }
