9100e980f5d1ec02ab6af04ff63a3d9d85e9e820
Vagusko <p.kolinko95@gmail.com>
2020-11-19 20:59:01 +0100

    maker add form

--- src/app/add/add-maker/add-maker.component.ts
+++ src/app/add/add-maker/add-maker.component.ts
@@ -1,4 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {MakerService} from '../../services/maker.service';
 
 @Component({
   selector: 'app-add-maker',
@@ -6,13 +8,33 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
   styleUrls: ['./add-maker.component.css']
 })
 export class AddMakerComponent implements OnInit {
-
+  addMakerForm: FormGroup;
+  Content = '';
   @Output() messageEvent = new EventEmitter<string>();
 
-  constructor() {
+  constructor(private makerService: MakerService, public fb: FormBuilder) {
+    this.addMakerForm = this.fb.group({
+      name: [],
+    });
   }
 
   ngOnInit(): void {
   }
 
+  setActiveContent(ActiveContent, contentType): void {
+    this.Content = ActiveContent + contentType;
+    this.messageEvent.emit(this.Content);
+  }
+
+  submitForm(): void {
+    this.postForm(() => {
+      this.setActiveContent('maker', '');
+    }, this.addMakerForm.value);
+  }
+
+  postForm(setActiveContent, formValues): void {
+    this.makerService.create(() => {
+      setActiveContent();
+    }, formValues);
+  }
 }
